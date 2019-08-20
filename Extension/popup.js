555a6e8065ba51f3a4fc6f164a0f132239b6b978
naseer2426 <35726629+naseer2426@users.noreply.github.com>
2019-08-20 20:50:42 +0800

    Working UI

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,9 +1,10 @@
-// ui.start('#firebaseui-auth-container', {
-//   signInOptions: [
-//     firebase.auth.EmailAuthProvider.PROVIDER_ID
-//   ]
-//   // Other config options...
-// });
+var firebaseConfig = {
+  apiKey: "AIzaSyDK3SCSl_LpVx5Fcac4PliMRIZA46E4zBs",
+  authDomain: "git-lance.firebaseapp.com",
+  projectId: "git-lance"
+};
+
+firebase.initializeApp(firebaseConfig);
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 
@@ -13,6 +14,7 @@ var uiConfig = {
       // User successfully signed in.
       // Return type determines whether we continue the redirect automatically
       // or whether we leave that to developer to handle.
+      console.log(authResult);
       return true;
     },
     uiShown: function() {
@@ -25,19 +27,14 @@ var uiConfig = {
   signInFlow: 'popup',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
   tosUrl: '<your-tos-url>',
   // Privacy policy url.
   privacyPolicyUrl: '<your-privacy-policy-url>'
 };
-console.log("LOL");
 $(document).ready(function(){
 	ui.start('#firebaseui-auth-container', uiConfig);
 	console.log("Running")
